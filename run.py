c24cef8b666e1da0521db78757dc807bcb710e31
MaiXaochai <maixiaochai@outlook.com>
2021-06-23 15:10:36 +0800

    feat:添加新的TODO内容 docs:添加requirements.txt方便使用

--- run.py
+++ run.py
@@ -186,9 +186,9 @@ def demo():
 if __name__ == '__main__':
     """
         TODO:
-            1. This session is in 'prepared' state; no further SQL can be emitted within this transaction 的解决
-                https://blog.csdn.net/weixin_43620243/article/details/108733967，这里提供了方案，自身是否提供了关闭的功能？
-                或者找出更好的解决方法
-            2. 用变量或者redis来减少对数据库的访问，加快速度
+            1. 用变量或者redis来减少对数据库的访问，加快速度
+            2. 数据库类的装饰器的添加
+            3. ORM类数据库连接池的抽象并建库
+            4. 部分依然出错的网址的处理和再分析
     """
     main()
