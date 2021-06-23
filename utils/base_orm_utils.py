6959d4d67dfcb17c95fba843484a45c88692741a
you <you@example.com>
2021-06-23 18:49:05 +0800

    fix:修复sqlalchemy版本不同导致的引入的BUG

--- utils/base_orm_utils.py
+++ utils/base_orm_utils.py
@@ -9,7 +9,7 @@
 ------------------------------------------
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import SingletonThreadPool
 
 from settings import cfg
