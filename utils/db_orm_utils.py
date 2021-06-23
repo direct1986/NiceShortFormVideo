2461bd4f36b9df7e4de18d516879993baa729c37
MaiXaochai <maixiaochai@outlook.com>
2021-06-23 14:48:10 +0800

    fix:修复线程中关闭数据库连接导致的SQL执行失败问题

--- utils/db_orm_utils.py
+++ utils/db_orm_utils.py
@@ -11,7 +11,6 @@
 from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from settings import cfg
 from utils.models import Videos, engine
 
 
@@ -23,15 +22,16 @@ class DataBase:
         session_factory = sessionmaker(bind=engine)
         self.session_safe = scoped_session(session_factory)
 
-    @property
-    def session(self):
+    def __get_session(self):
         return self.session_safe()
 
     def get_next_id(self) -> int:
         """获取最大值"""
-        row = self.session.query(self.tb_data).order_by(self.tb_data.id.desc()).first()
+        session = self.__get_session()
+        row = session.query(self.tb_data).order_by(self.tb_data.id.desc()).first()
         next_id = (row.id + 1) if row else 1
 
+        session.close()
         return next_id
 
     def insert(self, **kwargs):
@@ -43,39 +43,39 @@ class DataBase:
                 v = kwargs.get(k)
                 setattr(tb_data, k, v)
 
-        self.session.add(tb_data)
-        self.commit()
+        session = self.__get_session()
+        session.add(tb_data)
+        session.commit()
+        session.close()
 
     def update(self, row_id: int, size: float):
         """更新数据"""
-        row = self.session.query(self.tb_data).filter(self.tb_data.id == row_id).first()
+        session = self.__get_session()
+        row = session.query(self.tb_data).filter(self.tb_data.id == row_id).first()
         row.endOn = func.now()
         row.size = size
-        self.commit()
+        session.commit()
+        session.close()
 
     def has_data(self, md5_value: str) -> bool:
         """通过判断MD5值，确定视频是否存在"""
-        row = self.session.query(self.tb_data).filter(self.tb_data.md5 == md5_value).first()
+        session = self.__get_session()
+        row = session.query(self.tb_data).filter(self.tb_data.md5 == md5_value).first()
+        session.close()
 
         return True if row else False
 
     def has_url(self, url: str) -> bool:
         """视频链接是否存在"""
-        row = self.session.query(self.tb_data).filter(self.tb_data.url == url).first()
+        session = self.__get_session()
+        row = session.query(self.tb_data).filter(self.tb_data.url == url).first()
+        session.close()
+
         return True if row else False
 
     def fetch_all_hash_value(self):
-        rows = self.session.query(self.tb_data).with_entities(self.tb_data.md5).all()
+        session = self.__get_session()
+        rows = session.query(self.tb_data).with_entities(self.tb_data.md5).all()
+        session.close()
 
         return [x[0] for x in rows] or rows
-
-    def commit(self):
-        self.session.commit()
-
-    def close(self):
-        self.session.close()
-
-    def __del__(self):
-        # 将线程还给线程池
-        if "sqlite" not in cfg.db_uri.lower():
-            self.session.remove()
