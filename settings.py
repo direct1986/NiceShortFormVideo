4a1af072cde56a1ebc7812cf387d1ecf44d084bc
MaiXiaochai <maixiaochai@outlook.com>
2021-07-16 19:58:10 +0800

    feat：添加了一个备用的地址到下载列表

--- settings.py
+++ settings.py
@@ -46,12 +46,13 @@ class BaseConfig:
     urls = [
         "http://www.kuaidoushe.com/video.php",
         "https://tvv.tw/xjj/kuaishou/video.php",
-        "https://xjj.349457.xyz/video.php"
+        "https://xjj.349457.xyz/video.php",
+        "http://dou.plus/get/get1.php",  # 这个也只能下载一些
     ]
 
     # 备选的下载地址，这些下载地址或多或少有一些限制
     urls2 = [
-        "http://dou.plus/get/get1.php"  # 这个也只能下载一些
+        "http://dou.plus/get/get1.php",  # 这个也只能下载一些
         "http://wmsp.cc/video.php"  # 这个反爬虫，设置(2, 4)秒的随机sleep可解除
     ]
 
